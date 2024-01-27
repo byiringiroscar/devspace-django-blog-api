9c4d9c37b7ada0492107b2a6e6e8f526f2a79735
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-27 19:51:28 +0200

    Add data of blog and migrations

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import BlogListCreateAPIView, BlogRetrieveUpdateDestroyAPIView
 
 
+urlpatterns = [
+    path('blog/', BlogListCreateAPIView.as_view(), name='blog-list-create'),
+    path('blog/<uuid:id>/', BlogRetrieveUpdateDestroyAPIView.as_view(), name='blog-retrieve-update-destroy'),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import BlogSerializer
+from .models import Blog
 
 # Create your views here.
+
+
+class BlogListCreateAPIView(ListCreateAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+
+
+class BlogRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+    lookup_field = 'id'
