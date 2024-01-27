94e422f425a8c81209e55cb00fa65ab72147e61d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-27 19:22:31 +0200

    Ad blog model and route config

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+import uuid as uuid_lib
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+    title = models.CharField(max_length=100)
+    blog_image = models.ImageField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
